a17779ba9d8cc4137f42d8d144062bc4c50c9a8d
Ian O'Connor <ianpaoconnor@gmail.com>
2025-01-29 12:37:13 -0500

    Use real lambda arn

--- provisioning/base/resources.tf
+++ provisioning/base/resources.tf
@@ -31,7 +31,7 @@ resource "aws_s3_object" "uploaded_zip" {
 # Create the lambda:
 resource "aws_lambda_function" "lambda_instance" {
   description   = "A small service for determining if an item or bib is research"
-  function_name = "IsResearchService-${var.environment}-test"
+  function_name = "IsResearchService-${var.environment}"
   handler       = "app.handle_event"
   memory_size   = 128
   role          = "arn:aws:iam::946183545209:role/lambda-full-access"
